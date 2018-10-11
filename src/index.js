ce3400708c64e7bac044ed7f1fcbda802abfd2a2
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-11 18:09:05 -0500

    added posts_index component, removed app component, removed router test code

--- src/index.js
+++ src/index.js
@@ -3,28 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import PostsIndex from './components/posts_index';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-class Hello extends React.Component {
-  render() {return <div>Hello! <a href="/goodbye">bye</a></div>}
-};
-
-class GoodBye extends React.Component {
-  render() {return <div>Good-bye <a href="/hello">hi</a></div>}
-};
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello}>
-        </Route>
-        <Route path="/goodbye" component={GoodBye}>
-        </Route>
+        <PostsIndex />
       </div>
     </BrowserRouter>
   </Provider>
