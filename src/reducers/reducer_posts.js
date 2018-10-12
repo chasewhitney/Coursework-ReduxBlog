83f80474d0d6c4dbbf91512ff0c5308dc04e74e8
Chase Whitney <ChaseEWhitney@gmail.com>
2018-10-12 12:57:39 -0500

    removed log from and cleaned up reducer_posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,10 +3,8 @@ import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
-    case FETCH_POSTS:    
-    const data = _.mapKeys(action.payload.data, 'id');
-    console.log('reducer_posts returning:', data);
-      return data;
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
     default:
       return state;
   }
